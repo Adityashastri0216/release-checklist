df06a4e0735441356e64fb166912db923917424d
Aditya Shastri <aditya.s.shastri1817@gmail.com>
2026-07-26 12:33:35 +0530

    some changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://release-checklist-4gnm.onrender.com",
 });
 
 export default api;
